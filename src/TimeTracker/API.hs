cb11f766d79c13c2a9a17e00718c48e13d04e08a
Andres Loeh <andres@well-typed.com>
2017-02-24 16:49:21 +0100

    Add an API.

--- src/TimeTracker/API.hs
+++ src/TimeTracker/API.hs
@@ -1,3 +1,10 @@
 module TimeTracker.API where
 
+import Servant.API
 
+import TimeTracker.Library
+
+type TimeTrackerAPI =
+       "start"  :> Post '[JSON] NoContent
+  :<|> "stop"   :> Post '[JSON] NoContent
+  :<|> "status" :> Get '[JSON] TimeTrackerStatus
--- src/TimeTracker/Library.hs
+++ src/TimeTracker/Library.hs
@@ -1,22 +1,24 @@
 {-# LANGUAGE DeriveAnyClass #-}
 module TimeTracker.Library where
 
-import Data.Aeson
-import Data.Swagger
+-- import Data.Aeson
+-- import Data.Swagger
 import Data.Time
-import GHC.Generics
+-- import GHC.Generics
 
 data TimeTrackerStatus =
   TimeTrackerStatus
     { ttsTimeTracked :: NominalDiffTime
     , ttsMode        :: TimeTrackerMode
     }
-  deriving (Show, Generic, ToJSON, FromJSON, ToSchema)
+  deriving (Show)
+  -- deriving (Show, Generic, ToJSON, FromJSON, ToSchema)
 
 data TimeTrackerMode =
     Off
   | On { ttmSince :: UTCTime }
-  deriving (Show, Generic, ToJSON, FromJSON, ToSchema)
+  deriving (Show)
+  -- deriving (Show, Generic, ToJSON, FromJSON, ToSchema)
 
 initialTimeTrackerStatus :: TimeTrackerStatus
 initialTimeTrackerStatus =
